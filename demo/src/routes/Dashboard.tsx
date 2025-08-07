10d4ab3102a4cde4457800a5dcbe3dba867d7ee1
klmagpan <klmagpan@ucsc.edu>
2025-08-07 00:31:55 -0700

    Initial commit of demo application for Compiler Project course

---
+++ demo/src/routes/Dashboard.tsx
@@ -0,0 +1,17 @@
+import { Layout } from "../components/Layout"
+
+type DashboardRouteProps = {
+  username : string
+}
+
+export const Dashboard = ({
+  username = "Default User"
+} : DashboardRouteProps ) => (
+  <Layout isSignedIn={true}>
+    <div className="dashboard">
+      <h3>
+        You are signed in, {username}.
+      </h3>
+    </div>
+  </Layout>
+)
